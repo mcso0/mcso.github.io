d61f44c3c0d6a064ce8a73d3c8c2aa42fc513be9
mcso0 <102334964+mcso0@users.noreply.github.com>
2025-07-03 18:51:14 +0900

    job card 추가

--- app/features/jobs/components/job-card.tsx
+++ app/features/jobs/components/job-card.tsx
@@ -8,7 +8,7 @@ import {
 } from "../../../common/components/ui/card";
 import { Badge } from "../../../common/components/ui/badge";
 import { Button } from "../../../common/components/ui/button";
-import { CircleDollarSignIcon, MapPinIcon } from "lucide-react";
+import { CircleDollarSignIcon, MapPinIcon, ArrowRightIcon } from "lucide-react";
 
 export interface JobCardProps {
   id: string;
@@ -48,25 +48,27 @@ export function JobCard({
 
   return (
     <Link to={`/jobs/${id}`}>
-      <Card className="hover:bg-neutral-100 dark:hover:bg-neutral-900 cursor-pointer gap-4 transition-colors duration-200">
-        <CardHeader>
-          <div className="flex items-center gap-4 mb-8">
+      <Card className="hover:bg-neutral-100 dark:hover:bg-neutral-900 cursor-pointer gap-2 transition-colors duration-200">
+        <CardHeader className="flex flex-col gap-2">
+          {/* 회사 로고와 회사 이름, 게시일 */}
+          <div className="flex items-center gap-2 mb-2">
             <img
               src={company.logo}
               alt={`${company.name} Logo`}
-              className="size-10 rounded-full"
+              className="size-6 rounded-full"
             />
             <div className="space-x-2">
               <span className="text-accent-foreground">{company.name}</span>
               <span className="text-xs text-muted-foreground">{postedAt}</span>
             </div>
           </div>
+          {/* 직무 제목 */}
           <CardTitle className="flex flex-row justify-start items-end gap-2 leading-tight tracking-tight">
             <span className="text-accent-foreground">{title}</span>
           </CardTitle>
         </CardHeader>
 
-        <CardContent className="flex flex-row justify-start items-center gap-2">
+        <CardContent className="flex flex-row justify-start items-center gap-2 mb-2">
           <Badge variant="outline">{employmentType}</Badge>
           <Badge variant="outline">{workType}</Badge>
           {additionalBadges.map((badge, index) => (
@@ -76,7 +78,7 @@ export function JobCard({
           ))}
         </CardContent>
 
-        <CardFooter className="flex justify-between">
+        <CardFooter className="flex justify-between items-end gap-2">
           <div className="flex flex-col gap-1">
             <div className="flex flex-row justify-start items-center gap-2">
               <CircleDollarSignIcon className="size-4 text-muted-foreground" />
@@ -93,10 +95,11 @@ export function JobCard({
           </div>
           <Button
             variant="default"
-            className="flex flex-row gap-2 text-sm"
+            className="flex flex-row gap-1 text-sm cursor-pointer"
             size="sm"
           >
             Apply Now
+            <ArrowRightIcon className="size-4" />
           </Button>
         </CardFooter>
       </Card>
